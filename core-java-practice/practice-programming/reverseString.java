e052dde25da903e5050020b728b4a9b074e60676
Kapil Patel <scorpiankapil@gmail.com>
2026-06-11 09:57:46 +0530

    [Kapil Patel] Adding String Problem in practice-programming

--- core-java-practice/practice-programming/reverseString.java
+++ core-java-practice/practice-programming/reverseString.java
@@ -1,15 +1,33 @@
-public class reverseString {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+public class ReverseString {
+    public static void main(String[] args) {
 
         String original = "I Love Coding";
+
+        String[] arr = original.split("");
+
         String reversed = "";
 
-        for (int i = 0; i < original.length(); i++){
-
-            reversed = original.charAt(i) + reversed;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            reversed += arr[i];
         }
-        System.out.println(reversed);
 
+        System.out.println(reversed);
     }
 }
+
+
+// public class reverseString {
+//     public static void main(String[] args){
+//         System.out.println("Hello World!");
+
+//         String original = "I Love Coding";
+//         String reversed = "";
+
+//         for (int i = 0; i < original.length(); i++){
+
+//             reversed = original.charAt(i) + reversed;
+//         }
+//         System.out.println(reversed);
+
+//     }
+// }
